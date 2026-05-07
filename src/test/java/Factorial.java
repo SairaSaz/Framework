d5da477e6d71ad2ef4b740135da7fad8dec3c7e5
SairaSaz <saira.sazonova87@gmail.com>
2026-05-07 23:49:55 +0400

    D.Z.7

---
+++ src/test/java/Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+
+        public static long calculateLong(int n) {
+            if (n < 0) {
+                throw new IllegalArgumentException();
+            }
+            long result = 1;
+            for (int i = 2; i <= n; i++) {
+                result *= i;
+            }
+            return result;
+        }
+
+}
---
+++ src/test/java/FactorialTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+    class FactorialTest {
+
+        @Test
+        @DisplayName("Factorial 5 = 120")
+        void factorialFive() {
+            Assertions.assertEquals(120, Factorial.calculateLong(5));
+        }
+
+    }
