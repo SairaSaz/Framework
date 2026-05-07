8186d151197518b356d6031c61f7728f13c7726a
SairaSaz <saira.sazonova87@gmail.com>
2026-05-07 23:52:22 +0400

    D.Z.7

--- src/test/java/Factorial.java
+++ src/test/java/Factorial.java
@@ -1,14 +1,13 @@
 public class Factorial {
 
-        public static long calculateLong(int n) {
-            if (n < 0) {
-                throw new IllegalArgumentException();
-            }
-            long result = 1;
-            for (int i = 2; i <= n; i++) {
-                result *= i;
-            }
-            return result;
+    public static long calculateLong(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
         }
-
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -2,12 +2,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-    class FactorialTest {
+class FactorialTest {
 
-        @Test
-        @DisplayName("Factorial 5 = 120")
-        void factorialFive() {
-            Assertions.assertEquals(120, Factorial.calculateLong(5));
-        }
-
+    @Test
+    @DisplayName("Factorial 5 = 120")
+    void factorialFive() {
+        Assertions.assertEquals(120, Factorial.calculateLong(5));
     }
+}
