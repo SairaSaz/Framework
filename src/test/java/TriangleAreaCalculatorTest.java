d46b5f37f4393684f865b6a39961936722371eb0
SairaSaz <saira.sazonova87@gmail.com>
2026-05-08 20:47:00 +0400

    D.Z.7

--- src/test/java/TriangleAreaCalculatorTest.java
+++ src/test/java/TriangleAreaCalculatorTest.java
@@ -1,12 +1,12 @@
 package com.example;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 public class TriangleAreaCalculatorTest {
 
     @Test
     public void testArea() {
         double result = com.example.TriangleAreaCalculator.calculate(3, 4, 5);
-        assertEquals(result, 6.0, 0.0001);
+        Assert.assertEquals(result, 6.0, 0.0001);
     }
 }
